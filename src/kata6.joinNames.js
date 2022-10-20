272f0d5602c4c7811bd8ed6bf52fad6b37d015df
Carl Beckley <carlbeckley@gmail.com>
2022-10-20 23:36:19 +0100

    completed kata6

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,5 +1,7 @@
 const joinNames = (namesObj) => {
-
+  const firstNames = namesObj.map(({ name }) => name);
+  const lastName = firstNames.pop();
+  return `${firstNames.join(", ")} & ${lastName}`;
 };
 
 module.exports = joinNames;
